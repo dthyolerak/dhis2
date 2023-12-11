e20a4df73376cde41253a2e36dfa147dacec5c54
Donnex Thyolera Kamsonga <bed-com-15-19@unima.ac.mw>
2023-12-12 00:31:32 +0200

    Patient details page done designing

--- src/components/patients/patients.jsx
+++ src/components/patients/patients.jsx
@@ -2,7 +2,70 @@ import React from "react";
 import "./patients.css";
 
 const Patients = () => {
-  return <div>patients</div>;
+  return <div className="patients-cont">
+    <h1 className="title">Patients Details</h1>
+    <form className="search-patients">
+      <input type="search" name="" id="" />
+      <button>Search</button>
+    </form>
+    <div className="patients-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Message</th>
+            <th>Recipients</th>
+            <th>Date</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr >
+              <td>Hello there</td>
+              <td>0993585213</td>
+              <td>27 May 2015: 8:30AM</td>
+              <td className="missed">Missed</td>
+              <td className="viewDetails">View Details</td>
+            </tr>
+            <tr >
+              <td>Hello there</td>
+              <td>0993585213</td>
+              <td>27 May 2015: 8:30AM</td>
+              <td className="attend">attend</td>
+              <td className="viewDetails">View Details</td>
+            </tr>
+            <tr >
+              <td>Hello there</td>
+              <td>0993585213</td>
+              <td>27 May 2015: 8:30AM</td>
+              <td className="attend">attend</td>
+              <td className="viewDetails">View Details</td>
+            </tr>
+            <tr >
+              <td>Hello there</td>
+              <td>0993585213</td>
+              <td>27 May 2015: 8:30AM</td>
+              <td className="attend">attend</td>
+              <td className="viewDetails">View Details</td>
+            </tr>
+            <tr >
+              <td>Hello there</td>
+              <td>0993585213</td>
+              <td>27 May 2015: 8:30AM</td>
+              <td className="missed">Missed</td>
+              <td className="viewDetails">View Details</td>
+            </tr>
+            <tr >
+              <td>Hello there</td>
+              <td>0993585213</td>
+              <td>27 May 2015: 8:30AM</td>
+              <td className="attend">attend</td>
+              <td className="viewDetails">View Details</td>
+            </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>;
 };
 
 export default Patients;
